f4739785dfa2131c655628137f3c841d274ea10c
YuforiaCode <2628489679@qq.com>
2025-10-24 13:42:30 +0800

    实现向量库操作相关接口：文本转向量、内容搜索、搜索全部、删除文本

--- tj-aigc/src/main/java/com/tianji/aigc/controller/EmbeddingController.java
+++ tj-aigc/src/main/java/com/tianji/aigc/controller/EmbeddingController.java
@@ -4,11 +4,11 @@ import cn.hutool.core.collection.CollStreamUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.ai.document.Document;
+import org.springframework.ai.embedding.EmbeddingModel;
+import org.springframework.ai.embedding.EmbeddingResponse;
+import org.springframework.ai.vectorstore.SearchRequest;
 import org.springframework.ai.vectorstore.VectorStore;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,7 +19,12 @@ import java.util.List;
 public class EmbeddingController {
 
     private final VectorStore vectorStore;
+    private final EmbeddingModel embeddingModel;
 
+    /**
+     * 保存文本到向量库
+     * @param messages 文本列表
+     */
     @PostMapping
     public void saveVectorStore(@RequestParam("messages") List<String> messages) {
         log.info("保存到向量数据库中，消息数据：{}", messages);
@@ -30,6 +35,46 @@ public class EmbeddingController {
         //存储到向量数据库中
         this.vectorStore.add(documents);
         log.info("保存到向量数据库成功, 数量：{}", messages.size());
+    }
+
+    /**
+     * 文本转向量
+     * @param message 文本
+     * @return 向量
+     */
+    @GetMapping
+    public EmbeddingResponse embed(@RequestParam("message") String message) {
+        return this.embeddingModel.embedForResponse(List.of(message));
+    }
+
+    /**
+     * 删除文本
+     * @param ids 文本id列表
+     */
+    @DeleteMapping
+    public void deleteVectorStore(@RequestParam("ids") List<String> ids) {
+        // 删除向量数据库中的数据
+        this.vectorStore.delete(ids);
+    }
+
+    /**
+     * 内容搜索
+     * @param message 搜索内容
+     * @return 搜索结果
+     */
+    @GetMapping("/search")
+    public List<Document> search(@RequestParam("message") String message) {
+        return this.vectorStore.similaritySearch(SearchRequest.builder().query(message).topK(5).build());
+    }
+
+    /**
+     * 搜索全部
+     * @return 搜索结果
+     */
+    @GetMapping("/search/all")
+    public List<Document> searchAll() {
+        // 搜索全部数据
+        return this.vectorStore.similaritySearch(SearchRequest.builder().query("").topK(999).build());
     }
 
 }
