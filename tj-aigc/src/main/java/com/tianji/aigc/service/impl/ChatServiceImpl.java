0377a19f208dcd933e6e2221c7884e08bd91309e
YuforiaCode <2628489679@qq.com>
2025-10-24 10:45:10 +0800

    预下单

--- tj-aigc/src/main/java/com/tianji/aigc/service/impl/ChatServiceImpl.java
+++ tj-aigc/src/main/java/com/tianji/aigc/service/impl/ChatServiceImpl.java
@@ -10,6 +10,7 @@ import com.tianji.aigc.constants.Constant;
 import com.tianji.aigc.enums.ChatEventTypeEnum;
 import com.tianji.aigc.service.ChatService;
 import com.tianji.aigc.vo.ChatEventVO;
+import com.tianji.common.utils.UserContext;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.ai.chat.client.ChatClient;
@@ -45,7 +46,7 @@ public class ChatServiceImpl implements ChatService {
      * @param sessionId 会话id
      * @return 回答内容(文本内容和事件类型)
      * 流式结构说明：每行数据，都是一个json数据
-     * 流式对话 -> 应用system提示词 -> 会话记忆 -> 保存停止输出的记录 -> 查询课程 -> 展示课程卡片
+     * 流式对话 -> 应用system提示词 -> 会话记忆 -> 保存停止输出的记录 -> 查询课程 -> 展示课程卡片 -> 预下单
      */
     @Override
     public Flux<ChatEventVO> chat(String question, String sessionId) {
@@ -55,6 +56,8 @@ public class ChatServiceImpl implements ChatService {
         StringBuilder outputBuilder = new StringBuilder();
         // 生成请求id
         var requestId = IdUtil.fastSimpleUUID();
+        // 获取用户id
+        var userId = UserContext.getUser();
 
         return this.chatClient.prompt()
                 .system(promptSystem -> promptSystem
@@ -64,6 +67,7 @@ public class ChatServiceImpl implements ChatService {
                 .advisors(advisor -> advisor.param(AbstractChatMemoryAdvisor.CHAT_MEMORY_CONVERSATION_ID_KEY, conversationId))
                 .toolContext(MapUtil.<String, Object>builder() // 设置tool列表
                         .put(Constant.REQUEST_ID, requestId) // 设置请求id参数
+                        .put(Constant.USER_ID, userId) // 设置用户id参数
                         .build()
                 )
                 .user(question)
